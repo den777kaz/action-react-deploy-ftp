b05cf48698444144760679bda4aedb64400761c6
den777kaz <52310435+den777kaz@users.noreply.github.com>
2021-03-16 13:24:08 +0100

    idfgdfg dfgdg

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} className='App-logo' alt='logo' />
-        <p>welcome to github action</p>
+        <p> hallo world</p>
         <a
           className='App-link'
           href='https://reactjs.org'
